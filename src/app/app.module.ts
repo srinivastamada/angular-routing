dc17fe0ba4952573fffddf3f2dabecfe28a15d23
Srinivas Tamada <srinivas@9lessons.info>
2018-09-05 17:46:20 -0400

    New Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
