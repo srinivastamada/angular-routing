25137dcfb5143992e9e3e9c03dfd66e297172d21
Srinivas Tamada <srinivas@9lessons.info>
2018-09-06 14:28:26 -0400

    Home

---
+++ src/app/home/home.routes.ts
@@ -0,0 +1,14 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Route } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { SettingsComponent } from './settings/settings.component';
+export const HomeRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'settings', component: SettingsComponent }
+    ]
+  }
+];
