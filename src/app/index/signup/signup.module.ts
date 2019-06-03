6c397f42b67f4b856e55075fd572be20a820c227
Srinivas Tamada <srinivas@9lessons.info>
2019-06-02 23:40:20 -0400

    Routing

--- src/app/index/signup/signup.module.ts
+++ src/app/index/signup/signup.module.ts
@@ -4,10 +4,10 @@ import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '../../components/components.module';
 import { SignupComponent } from './signup.component';
 import { SignupRoutes } from './signup.routes';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [SignupComponent],
   exports: [SignupComponent],
-  imports: [RouterModule.forChild(SignupRoutes), CommonModule, ComponentsModule]
+  imports: [RouterModule.forChild(SignupRoutes), CommonModule,FormsModule, ComponentsModule]
 })
 export class SignupModule {}
