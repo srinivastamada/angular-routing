7779ed90b8d89840ded53a9aa951b03f0a2f6fa9
Srinivas Tamada <srinivas@9lessons.info>
2021-11-11 23:46:32 -0500

    A

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,8 @@
+describe('Login page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200')
+      })
+    it('should display the login page', () => {
+        cy.include
+    });
+});
